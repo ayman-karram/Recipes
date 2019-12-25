55fd8a2e6ef41f222dbdce08de7d8de9a1b22da7
ayman-karram <ayman.karram@gmail.com>
2019-12-25 20:09:46 +0100

    Created Bindable class to used to bind Recipes view Model data into view controller

---
+++ Recipes/Application/Helpers/Bindable.swift
@@ -0,0 +1,33 @@
+//
+//  Bindable.swift
+//  Recipes
+//
+//  Created by Ayman Karram on 25.12.19.
+//  Copyright © 2019 Ayman Karram. All rights reserved.
+//
+
+import Foundation
+
+class Bindable<T> {
+    typealias Listener = ((T) -> Void)
+    var listener: Listener?
+
+    var value: T {
+        didSet {
+            listener?(value)
+        }
+    }
+
+    init(_ v: T) {
+        self.value = v
+    }
+
+    func bind(_ listener: Listener?) {
+        self.listener = listener
+    }
+
+    func bindAndFire(_ listener: Listener?) {
+        self.listener = listener
+        listener?(value)
+    }
+}
