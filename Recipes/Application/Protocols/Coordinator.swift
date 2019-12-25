0cf034ca53319e45d2047f66f27fb2bd901af28c
ayman-karram <ayman.karram@gmail.com>
2019-12-25 21:33:34 +0100

    Created Coordinator protocol

---
+++ Recipes/Application/Protocols/Coordinator.swift
@@ -0,0 +1,14 @@
+//
+//  Coordinator.swift
+//  Recipes
+//
+//  Created by Ayman Karram on 25.12.19.
+//  Copyright © 2019 Ayman Karram. All rights reserved.
+//
+
+import UIKit
+protocol Coordinator {
+    var  navigationController: UINavigationController { get set }
+    func getViewController() -> UIViewController
+    func show(present: Bool)
+}
--- Recipes/Network/Helpers/Result.swift
+++ Recipes/Network/Helpers/Result.swift
@@ -12,8 +12,3 @@ enum RecipesResult<T, U: Error> {
     case success(payload: T)
     case failure(U?)
 }
-
-enum EmptyResult<U: Error> {
-    case success
-    case failure(U?)
-}
