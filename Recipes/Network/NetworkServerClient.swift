740fd72401816058c9cbd848f2cfd877fd8db512
ayman-karram <ayman.karram@gmail.com>
2019-12-25 17:25:57 +0100

    Created NetworkServerClient class to be responsible of calling APIs for fetching data using Contentful framework

---
+++ Recipes/Network/NetworkServerClient.swift
@@ -0,0 +1,19 @@
+//
+//  NetworkServerClient.swift
+//  Recipes
+//
+//  Created by Ayman Karram on 25.12.19.
+//  Copyright © 2019 Ayman Karram. All rights reserved.
+//
+
+import Foundation
+import Contentful
+
+class NetworkServerClient {
+
+    let client = Client(spaceId: "kk2bw5ojx476",
+    accessToken: "7ac531648a1b5e1dab6c18b0979f822a5aad0fe5f1109829b8a197eb2be4b84c")
+
+    
+
+}
